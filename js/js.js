59f427f1560a532611583b12e59f8530f15768b7
Lev <LevLeveofanych@gmail.com>
2016-03-02 14:57:22 +0300

    added </p>

--- js/js.js
+++ js/js.js
@@ -77,7 +77,13 @@ jQuery(document).ready(function(){
 		setTimeout(function(){
 			$("ul").animate({opacity: "1.0"}, 500);
 		}, 1000);
-		
+		}, 1000);
+		setTimeout(function(){
+			$("body").append("<p>");
+			$("body p").attr("class","text");
+			$("body p").html("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. ");
+			$("body p").animate({ opacity: "1.0"}, 1000);
+		}, 1700);
 		
 		setTimeout(function(){
 			$('img[name="back"]').animate({opacity: "1.0"}, 1000);
